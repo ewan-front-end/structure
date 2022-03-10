104051357c453e93527b724d1b35932d1f23255d
wanyuaning <wanyuaning@163.com>
2022-03-10 18:27:18 +0800

    配置完善

--- vuepress/.vuepress/config.js
+++ vuepress/.vuepress/config.js
@@ -1,4 +1,39 @@
 module.exports = {
+    "title": "标题文本",
+    "description": "说明文本",
+    "themeConfig": {
+        "port": "9000",
+        "logo": "/logo.png",
+        "nav": [
+            {
+                "text": "首页",
+                "link": "/"
+            },
+            {
+                "text": "工具",
+                "link": "/tools/"
+            },
+            {
+                "text": "项目",
+                "link": "/projects/"
+            },
+            {
+                "text": "场景",
+                "link": "/scene"
+            }
+        ],
+        "sidebarDepth": 2,
+        "sidebar": "auto"
+    },
+    "configureWebpack": {
+        "resolve": {
+            "alias": {
+                "@res": "resources",
+                "@res_md": "resources/md"
+            }
+        }
+    },
+    "debug": true,
     plugins: [
         require('./vuepress-plugin-super-block'), // path.resolve(__dirname, './vuepress-plugin-super-block/index.js')
     ]
