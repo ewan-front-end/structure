a348012e2621c4bf9355cee74515224435bd37b8
wanyuaning <wanyuaning@163.com>
2022-05-01 22:58:35 +0800

    a

--- vuepress/assets/update-assets.js
+++ vuepress/assets/update-assets.js
@@ -1,11 +1,10 @@
 const path = require("path")
 const { copySync } = require('../.utils/src/fs.js')
+const { DEPLOY_ASSETS } = require('../.structure/vuepress/deploy.js')
 const CWD = process.cwd();
 
-([
-    ['.structure/vuepress/assets/favicon.ico     ', 'docs/.vuepress/public/favicon.ico']
-]).forEach(res => {
+DEPLOY_ASSETS.forEach(res => {
     let from = path.join(CWD, res[0].trim()), to = path.join(CWD, res[1].trim())
     copySync(from, to)
-    console.log(res[0] + ' > ' + res[1])
+    console.log(res[0] + ' > ' + res[1] + '  ' + res[2])
 })
