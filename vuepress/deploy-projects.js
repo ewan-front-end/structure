2dd601a503a02dfeed8eb102a47b2eca5d52db58
wanyuaning <wanyuaning@163.com>
2022-06-04 21:01:28 +0800

    a

--- vuepress/deploy-projects.js
+++ vuepress/deploy-projects.js
@@ -4,33 +4,41 @@ const chalk = require('chalk')
 const { readFile, writeFile } = require('../.utils/src/fs.js')
 const parseCustomBlock = require('../.data/components/parseCustomBlock')
 const ARG_ARR = process.argv.slice(2)  // 命令参数
+const dirFrom = Path.resolve(__dirname, '../.data/projects')
+const dirTo = Path.resolve(__dirname, '../projects')
 
-const createDocs = () => {
-    const from = Path.resolve(__dirname, '../.data/projects')
-    const files = fs.readdirSync(from)
-    const docsDir = Path.resolve(__dirname, '../projects')
-    const indexPath = Path.resolve(docsDir, 'README.md')
-    let fileLinkStr = ''
-    files.forEach((file, index) => {
-        const to = Path.resolve(docsDir, file)
-        const name = file.replace('.md', '')
-        let info = {}
-        let content = readFile(Path.resolve(from, file));
-        if (/(^\{[^\}]+\})/.exec(content) !== null) {
-            info = JSON.parse(RegExp.$1)
-            let infoHtml = ''
-            info.title && (infoHtml += `############ ${info.title}`)
-            content = content.replace(RegExp.$1, infoHtml)
-        }
-        fileLinkStr += `- [${info.title || name}](${name})\n`
-        content = parseCustomBlock(content)
-        writeFile(to, `<pre class="code-block">${content}</pre>`, e => {
-            console.log(chalk.gray(to))
+const handleContent = (filename) => {
+    const pathTo = Path.resolve(dirTo, filename)
+    const name = filename.replace('.md', '')
+    let info = {}
+    let content = readFile(Path.resolve(dirFrom, filename));
+    if (/(^\{[^\}]+\})/.exec(content) !== null) {
+        info = JSON.parse(RegExp.$1)
+        let infoHtml = ''
+        info.title && (infoHtml += `############ ${info.title}`)
+        content = content.replace(RegExp.$1, infoHtml)
+    }
+    content = parseCustomBlock(content)
+    writeFile(pathTo, `<pre class="code-block">${content}</pre>`, e => {
+        console.log(chalk.gray(pathTo))
+    })
+    return `- [${info.title || name}](${name})\n`
+}
+const createDocs = (dest) => {
+    if (dest) {
+        if (!dest.includes('.md')) dest += '.md'
+        handleContent(dest)
+    } else {
+        const files = fs.readdirSync(dirFrom)
+        const readmePath = Path.resolve(dirTo, 'README.md')
+        let linkHtml = ''
+        files.forEach(filename => {
+            linkHtml += handleContent(filename)
         })
-    })
-    writeFile(indexPath, fileLinkStr, e => {
-        console.log(chalk.gray(indexPath))
-    })
+        writeFile(readmePath, linkHtml, e => {
+            console.log(chalk.gray(readmePath))
+        })
+    }
 }
 
 if (ARG_ARR.length > 0) {
@@ -42,6 +50,6 @@ if (ARG_ARR.length > 0) {
         writeFile(Path.resolve(__dirname, '../.data/projects/demo.md'), content)
     }
     if (arg === 'DOCS') {
-        createDocs()
+        ARG_ARR.length > 1 ? createDocs(ARG_ARR[1]) : createDocs()
     }
 }
