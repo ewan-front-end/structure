72eb9617dd083a11d876cd73a1a31e26e85a84bb
wanyuaning <wanyuaning@163.com>
2022-06-07 14:52:46 +0800

    1

--- vuepress/maps.js
+++ vuepress/maps.js
@@ -8,26 +8,35 @@ const DATA = [
 const THEME = [
 
 ]
-const FIRST_DEPLOY = [
-    ['.structure/vuepress/config.js         ', '.config.js                  ', '全局配置'],
-    ['.structure/vuepress/.data             ', '.data                       ', '数据体系'],
-    ['.structure/vuepress/.vuepress         ', '.vuepress                   ', '默认主题改造'],
-    ['.structure/vuepress/assets/favicon.ico', '.vuepress/public/favicon.ico', '资源:网页标签图标'],
-    ['.structure/vuepress/assets/logo.png   ', '.vuepress/public/logo.png   ', '资源:站点标识'],
-    ['.structure/vuepress/guidance.js       ', '.deploy/guidance.js         ', '更新引导'],
-    ['.structure/vuepress/update.js         ', '.deploy/update.js           ', '更新:数据体系/默认主题/静态资源'],
-    ['.structure/vuepress/deploy-projects.js', '.deploy/deploy-projects.js  ', '特种文档：项目描述体系部署']
+const DEPLOY_INIT = [
+    {type: 'COPY', from: '.structure/vuepress/config.js', to: '.config.js', desc: '全局配置'},
+    {type: 'COPY', from: '.structure/vuepress/.data', to: '.data', desc: '数据体系'},
+    {type: 'COPY', from: '.structure/vuepress/.vuepress', to: '.vuepress', desc: '默认主题改造'},
+    {type: 'COPY', from: '.structure/vuepress/assets/favicon.ico', to: '.vuepress/public/favicon.ico', desc: '资源:网页标签图标'},
+    {type: 'COPY', from: '.structure/vuepress/assets/logo.png', to: '.vuepress/public/logo.png', desc: '资源:站点标识'},
+    {type: 'COPY', from: '.structure/vuepress/guidance.js', to: '.deploy/guidance.js', desc: '更新引导'},
+    {type: 'COPY', from: '.structure/vuepress/update.js', to: '.deploy/update.js', desc: '更新:数据体系/默认主题/静态资源'},
+    {type: 'COPY', from: '.structure/vuepress/deploy-projects.js', to: '.deploy/deploy-projects.js', desc: '特种文档：项目描述体系部署'},
+
+    {type: 'SCRIPT', key: 'docs:dev', value: 'concurrently \"npm run data:watch\" \"npm run res:watch\" \"vuepress dev docs\"', desc: ''},
+    {type: 'SCRIPT', key: 'update:data', value: 'node docs/.deploy/update.js DATA', desc: '部署数据体系'},
+    {type: 'SCRIPT', key: 'update:theme', value: 'node docs/.deploy/update.js THEME', desc: '默认主题改造'},
+    {type: 'SCRIPT', key: 'update:assets', value: 'node docs/.deploy/update.js ASSETS', desc: '更新资源'},
+    {type: 'SCRIPT', key: 'data:create', value: 'node docs/.data/data-create.js', desc: '创建DATA到MD'},
+    {type: 'SCRIPT', key: 'data:watch', value: 'node docs/.data/data-watch.js', desc: '监听数据变化创建DATA到MD'},
+    {type: 'SCRIPT', key: 'res:create', value: 'node docs/.data/res-create.js', desc: '创建MD到DOC'},
+    {type: 'SCRIPT', key: 'res:watch', value: 'node docs/.data/res-watch.js', desc: '监听MD变化创建MD到DOC'}
 ]
 const UNDEPLOY = [
-    '.config.js',
-    {dir: '.data', exclude: ['data.js', 'md']}, // 排除删除
-    '.vuepress',
-    '.deploy/guidance.js',
-    '.deploy/update.js'
+    {type: 'DEL', file: '.config.js'},
+    {type: 'DEL', dir: '.data', exclude: ['data.js', 'md']},
+    {type: 'DEL', dir: '.vuepress'},
+    {type: 'DEL', file: '.deploy/guidance.js'},
+    {type: 'DEL', file: '.deploy/update.js'}
 ]
 
 module.exports = {
-    FIRST_DEPLOY,
+    DEPLOY_INIT,
     UNDEPLOY,
     ASSETS,
     DATA,
