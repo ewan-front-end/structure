10a988d357c925c0594dfa2a5fcdd4607c87f15f
wanyuaning <wanyuaning@163.com>
2022-06-09 15:02:26 +0800

    a

--- vuepress/deploy.js
+++ vuepress/deploy.js
@@ -1,33 +1,20 @@
-const fs = require('fs')
 const chalk = require('chalk')
 const path = require("path")
 const { DEPLOY_INIT } = require('./maps.js')
 const { confirm } = require('../../.utils/src/node/inquirer-prompt')
 const { copySync, editJson } = require('../../.utils/src/fs.js')
-const root = path.resolve(__dirname, '../..')
-const COPYS = [], SCRIPTS = []
-
-const fillStr = (str, len) => `${str}                                        `.substr(0, len)
-function printDeployList(arr) {
-    arr.forEach(item => {
-        const {type, from, to, key, value, file, dir, exclude, desc} = item
-        let str = chalk.gray(fillStr(type, 10))
-        if (type === 'COPY' && from && to) {
-            str += chalk.gray(fillStr(from, 40))
-            str += chalk.gray(fillStr(to, 30))
-            str += chalk.gray(desc)
-        }
-        if (type === 'SCRIPT') {}
-        console.log(str);
-    })
-}
+const ROOT_DOCS = path.resolve(__dirname, '../../..')
+const ROOT_ABST = path.resolve(__dirname, '../..')
 
 printDeployList(DEPLOY_INIT)
 confirm('是否继续？', false).then(bl => {
     bl && deploy()
 })
 
+const fillStr = (str, len) => `${str}                                        `.substr(0, len)
 function deploy() {
+    const COPYS = [], SCRIPTS = []
+
     DEPLOY_INIT.forEach(item => {
         if (item.type === 'COPY') COPYS.push(item)
         if (item.type === 'SCRIPT') SCRIPTS.push(item)
@@ -35,8 +22,8 @@ function deploy() {
 
     COPYS.forEach(item => {
         const { from, to, desc } = item
-        copySync(path.resolve(root, from), path.resolve(root, to))
-        console.log(chalk.gray('部署 docs/' + from + ' 到 docs/') + chalk.white(to) + chalk.gray('  ' + desc))
+        copySync(path.resolve(ROOT_ABST, from), path.resolve(ROOT_DOCS, to))
+        console.log(chalk.gray('部署 ' + from + ' 到 docs/') + chalk.white(to) + chalk.gray('  ' + desc))
     })
     
     if (SCRIPTS.length > 0) {
@@ -53,6 +40,19 @@ function deploy() {
             console.log(chalk.green('--------------------\n'))
         })
     }
+}
+function printDeployList(arr) {
+    arr.forEach(item => {
+        const {type, from, to, key, value, file, dir, exclude, desc} = item
+        let str = chalk.gray(fillStr(type, 10))
+        if (type === 'COPY' && from && to) {
+            str += chalk.gray(fillStr(from, 40))
+            str += chalk.gray(fillStr(to, 30))
+            str += chalk.gray(desc)
+        }
+        if (type === 'SCRIPT') {}
+        console.log(str);
+    })
 }
 
 
