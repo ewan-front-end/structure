57465948423aff3df8b953efcf5e7000de47d6f5
wanyuaning <wanyuaning@163.com>
2022-06-09 14:33:55 +0800

    1

--- vuepress/deploy.js
+++ vuepress/deploy.js
@@ -1,84 +1,64 @@
 const fs = require('fs')
 const chalk = require('chalk')
 const path = require("path")
-const root = path.resolve(__dirname, '..')
+const { DEPLOY_INIT } = require('./maps.js')
+const { confirm } = require('../../.utils/src/node/inquirer-prompt')
+const { copySync, editJson } = require('../../.utils/src/fs.js')
+const root = path.resolve(__dirname, '../..')
+const COPYS = [], SCRIPTS = []
 
-const { getStructure, getUtils } = require('./download.js')
-const deployed = fs.existsSync(path.resolve(__dirname, 'guidance.js'))
-
-const deployInit = () => {
-    const { copySync } = require('../.utils/src/fs.js')
-    const { DEPLOY_INIT } = require('../.structure/vuepress/maps.js')
-    const { editJson } = require('../.utils/src/fs.js')
-    const SCRIPTS = []
-
-    DEPLOY_INIT.forEach(item => {
-        if (item.type === 'COPY') {
-            const { from, to, desc } = item
-            copySync(path.resolve(root, from), path.resolve(root, to))
-            console.log(chalk.gray('部署 docs/' + from + ' 到 docs/') + chalk.white(to) + chalk.gray('  ' + desc))
+const fillStr = (str, len) => `${str}                                        `.substr(0, len)
+function printDeployList(arr) {
+    arr.forEach(item => {
+        const {type, from, to, key, value, file, dir, exclude, desc} = item
+        let str = chalk.gray(fillStr(type, 10))
+        if (type === 'COPY' && from && to) {
+            str += chalk.gray(fillStr(from, 40))
+            str += chalk.gray(fillStr(to, 30))
+            str += chalk.gray(desc)
         }
+        if (type === 'SCRIPT') {}
+        console.log(str);
+    })
+}
+
+printDeployList(DEPLOY_INIT)
+confirm('是否继续？', false).then(bl => {
+    bl && deploy()
+})
+
+function deploy() {
+    DEPLOY_INIT.forEach(item => {
+        if (item.type === 'COPY') COPYS.push(item)
         if (item.type === 'SCRIPT') SCRIPTS.push(item)
     })
 
-    if (SCRIPTS.length === 0) return
-    editJson(path.join(process.cwd(), 'package.json'), pkg => {
-        SCRIPTS.forEach(item => {
-            pkg.scripts[item.key] = item.value
-        })
-    }, path => {
-        console.log(chalk.gray('\n修改 ' + path))
-        console.log(chalk.green('--------------------'))
-        SCRIPTS.forEach(item => {
-            console.log(chalk.green(item.key) + item.desc)
-        })
-        console.log(chalk.green('--------------------\n'))
+    COPYS.forEach(item => {
+        const { from, to, desc } = item
+        copySync(path.resolve(root, from), path.resolve(root, to))
+        console.log(chalk.gray('部署 docs/' + from + ' 到 docs/') + chalk.white(to) + chalk.gray('  ' + desc))
     })
-}
-
-
-if (deployed) {
-    const { radio } = require('../.utils/src/node/inquirer-prompt')
-    const { delDest, delDirExc } = require('../.utils/src/fs.js')
-    const { UNDEPLOY } = require('../.structure/vuepress/maps.js')
-    const { copySync } = require('../.utils/src/fs.js')
-    radio('此项已部署过文档！', [{ value: 1, name: '引导重新部署' }, { value: 2, name: '清理并重新部署' }]).then(value => {
-        if (value === 1) require('./guidance.js')
-        if (value === 2) {
-            const date = new Date()
-            const hash = `${date.getFullYear()}${date.getMonth() + 1}${date.getDate()}-${date.getHours()}${date.getMinutes()}`
-            copySync(path.resolve(__dirname, '..', '.data/data.js'), path.join(__dirname, `.beifen/${hash}-data.js`))
-            copySync(path.resolve(__dirname, '..', '.data/md'), path.join(__dirname, `.beifen/${hash}-md`))
-
-            UNDEPLOY.forEach(item => {
-                if (item.type === 'DEL') {
-                    const { file, dir, exclude } = item
-                    if (file) {
-                        let dest = path.resolve(root, file)
-                        delDest(dest)
-                        console.log(chalk.gray('删除 docs/' + file))
-                    } else {
-                        let dest = path.resolve(root, dir)
-                        delDirExc(dest, exclude)
-                    }
-                }
+    
+    if (SCRIPTS.length > 0) {
+        editJson(path.join(process.cwd(), 'package.json'), pkg => {
+            SCRIPTS.forEach(item => {
+                pkg.scripts[item.key] = item.value
             })
-            delDest(path.resolve(__dirname, '../.structure'))
-            delDest(path.resolve(__dirname, '../.utils'))
-            delDest(path.resolve(__dirname, '../.data'))
-            console.log('\n')
-            console.log('.data/md      已备份到 .deploy/.benfen/md')
-            console.log('.data/data.js 已备份到 .deploy/.benfen/data.js')
-            console.log('\n')
-            console.log(chalk.green('全新部署：npm run deploy'))
-            console.log('\n')
-        }
-    })
-} else {
-    Promise.all([getStructure(), getUtils()]).then(arr => {
-        deployInit()
-    })
+        }, path => {
+            console.log(chalk.gray('\n修改 ' + path))
+            console.log(chalk.green('--------------------'))
+            SCRIPTS.forEach(item => {
+                console.log(chalk.green(item.key) + item.desc)
+            })
+            console.log(chalk.green('--------------------\n'))
+        })
+    }
 }
+
+
+
+
+
 
 // require('child_process').exec(`node ${path.resolve(__dirname, '../.data/res-create.js')} index`, function (error, stdout, stderr) {
 //     error && console.log(error)
--- vuepress/guidance.js
+++ vuepress/guidance.js
@@ -22,3 +22,40 @@ function deployStructure() {
         }
     })
 }
+
+// const { radio } = require('../.utils/src/node/inquirer-prompt')
+//     const { delDest, delDirExc } = require('../.utils/src/fs.js')
+//     const { UNDEPLOY } = require('../.structure/vuepress/maps.js')
+//     const { copySync } = require('../.utils/src/fs.js')
+//     radio('此项已部署过文档！', [{ value: 1, name: '引导重新部署' }, { value: 2, name: '清理并重新部署' }]).then(value => {
+//         if (value === 1) require('./guidance.js')
+//         if (value === 2) {
+//             const date = new Date()
+//             const hash = `${date.getFullYear()}${date.getMonth() + 1}${date.getDate()}-${date.getHours()}${date.getMinutes()}`
+//             copySync(path.resolve(__dirname, '..', '.data/data.js'), path.join(__dirname, `.beifen/${hash}-data.js`))
+//             copySync(path.resolve(__dirname, '..', '.data/md'), path.join(__dirname, `.beifen/${hash}-md`))
+
+//             UNDEPLOY.forEach(item => {
+//                 if (item.type === 'DEL') {
+//                     const { file, dir, exclude } = item
+//                     if (file) {
+//                         let dest = path.resolve(root, file)
+//                         delDest(dest)
+//                         console.log(chalk.gray('删除 docs/' + file))
+//                     } else {
+//                         let dest = path.resolve(root, dir)
+//                         delDirExc(dest, exclude)
+//                     }
+//                 }
+//             })
+//             delDest(path.resolve(__dirname, '../.structure'))
+//             delDest(path.resolve(__dirname, '../.utils'))
+//             delDest(path.resolve(__dirname, '../.data'))
+//             console.log('\n')
+//             console.log('.data/md      已备份到 .deploy/.benfen/md')
+//             console.log('.data/data.js 已备份到 .deploy/.benfen/data.js')
+//             console.log('\n')
+//             console.log(chalk.green('全新部署：npm run deploy'))
+//             console.log('\n')
+//         }
+//     })
