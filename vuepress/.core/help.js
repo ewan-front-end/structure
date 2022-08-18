90e8248fe3afc4cbc3ef667564e362c0861848d5
wanyuaning <wanyuaning@163.com>
2022-08-18 16:47:31 +0800

    1

--- vuepress/.core/help.js
+++ vuepress/.core/help.js
@@ -4,15 +4,16 @@ const { iterateList } = require('./utils')
 iterateList(HELP, 0)
 
 const table =
-    `┌──────────┬───────────────────────────┬───────────────────────────┬────────────────────────────┐
+    `
+┌──────────┬───────────────────────────┬───────────────────────────┬────────────────────────────┐
 │   类型               位置                         相关操作                    说明
-│ DOCS       docs/                                                    文档输出位置
-│ DATA       docs/.abstract/data.js      data:watch                   数据监听目标
-│                                        data:build [/tools]          把数据指向的RES构建到DOCS
-│ RES        docs/.abstract/md           res:watch                    资源监听目标
-│                                        res:build [demo]             构建资源到DOCS
-│ PREJECTS   docs/.abstract/projects     projects:deploy              项目监听目标
-│                                        projects:build [demo.md]     构建项目到DOCS
+│ DOCS       docs/
+│ DATA       docs/.abstract/data.js      data:watch                   监听位置文件
+│                                        data:build [/tools]          把DATA指向的RES构建到DOCS
+│ RES        docs/.abstract/md           res:watch                    监听位置目标
+│                                        res:build [demo]             构建RES到DOCS
+│ PREJECTS   docs/.abstract/projects     projects:deploy 
+│                                        projects:build [demo.md]
 └──────────┴───────────────────────────┴───────────────────────────┴────────────────────────────┘`
 console.log(chalk.gray(table) + '\n')
 
@@ -21,5 +22,6 @@ const firstTimeUseText = `
     比如第一次克隆到本地
     1. sudo npm i concurrently -g
     2. npm run data:build
+    3. npm run projects:build
 `
 console.log(chalk.gray(firstTimeUseText) + '\n')
