23a7a9d797516c7803ab90fded9b724bbc8eda5f
ewan <ewan@gmail.com>
2022-12-21 14:41:28 +0800

    折叠详情 添加内容样式

--- vuepress/.core/components/widgets/detail.js
+++ vuepress/.core/components/widgets/detail.js
@@ -1,11 +1,14 @@
 const { regexpPresetParse } = require('../regexp-preset')
 const REG_STYLE_STR = `(\\{[\\w\\s-;:'"#]+\\})?` // color: #f00; font-size: 14px
 /**
- * Detail
- * 突出简介隐藏详情
+ * 折叠详情
  * -----------
- * 标题 ▾ 说明
- *   ↧↥
+ * 普通 ▾
+ * ↧Detail Content↥
+ * 标题样式 ▾{color:#3ac}
+ * ↧Detail Content↥
+ * 内容样式 ▾
+ * {color:#3ac}↧Detail Content↥
  * -----------
  * 事件处理：.vuepress/theme/layouts/Layout.vue
     mounted () {
@@ -20,15 +23,16 @@ const REG_STYLE_STR = `(\\{[\\w\\s-;:'"#]+\\})?` // color: #f00; font-size: 14px
     */
 module.exports = code => {
     const REG_DETAIL_STR = regexpPresetParse([
-        { DETAIL_FORMAT: [{ DETAIL_INDENT: `\\x20*` }, { TITLE: `.+?` }, { SPACE: `\\s+` }, `▾`, { STYLE: REG_STYLE_STR }, { COMMENT: `[^\\n]*` }, `[\\r\\n]`, { CONTENT_INDENT: `\\x20*` }, `↧`, { CONTENT: `[^↥]+` }, `↥`] }
+        { DETAIL_FORMAT: [{ DETAIL_INDENT: `\\x20*` }, { TITLE: `.+?` }, { SPACE: `\\s+` }, `▾`, { STYLE: REG_STYLE_STR }, { COMMENT: `[^\\n]*` }, `[\\r\\n]`, { CONTENT_INDENT: `\\x20*` }, { CONTENT_STYLE: REG_STYLE_STR }, `↧`, { CONTENT: `[^↥]+` }, `↥`] }
     ])
     const REG_DETAIL = new RegExp(REG_DETAIL_STR.value)
 
     let detailMatch
     while ((detailMatch = REG_DETAIL.exec(code)) !== null) {
-        let { DETAIL_FORMAT, DETAIL_INDENT, TITLE, SPACE, STYLE, COMMENT, CONTENT_INDENT, CONTENT } = detailMatch.groups, descStyle = 'class="detail-desc"'
+        let { DETAIL_FORMAT, DETAIL_INDENT, TITLE, SPACE, STYLE, COMMENT, CONTENT_INDENT, CONTENT_STYLE, CONTENT } = detailMatch.groups, descStyle = 'class="detail-desc"', contentStyle = ''
         if (STYLE) descStyle += ` style="${STYLE.replace('{', '').replace('}', '')}"`
-        code = code.replace(DETAIL_FORMAT, `<div class="fold-detail sty${SPACE.length}">${DETAIL_INDENT}<span ${descStyle}>${TITLE}</span><span class="comment">${COMMENT}</span><div class="detail-content">${CONTENT_INDENT}<span>${CONTENT}</span></div></div>`)
+        if (CONTENT_STYLE) contentStyle = ` style="${CONTENT_STYLE.replace('{', '').replace('}', '')}"`
+        code = code.replace(DETAIL_FORMAT, `<div class="fold-detail sty${SPACE.length}">${DETAIL_INDENT}<span ${descStyle}>${TITLE}</span><span class="comment">${COMMENT}</span><div class="detail-content">${CONTENT_INDENT}<span${contentStyle}>${CONTENT}</span></div></div>`)
     }
     return code
 }
